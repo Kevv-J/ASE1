9a4b74aca19c101c257afcbc4d0118ece2084354
Nehul Rangappa <nehul.r17@iiits.in>
2018-11-18 20:55:41 +0530

    removed old files

--- ase_project/my_part/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyPartConfig(AppConfig):
-    name = 'my_part'
--- ase_project/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\dell\PycharmProjects\ase_project\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
