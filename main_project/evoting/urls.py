75f31f185f066dac4f80abe0276a0107c7c2c894
Kevv-J <kevinjohn1999@gmail.com>
2018-12-08 01:28:58 +0530

    Added Password Reset

--- main_project/evoting/urls.py
+++ main_project/evoting/urls.py
@@ -14,16 +14,20 @@ urlpatterns = [
     path('profile/', user_views.profile, name='evoting-user-profile'),
     path('activate/<uidb64>/<token>', user_views.activate, name='activate'),
     path('password_reset/', auth_views.PasswordResetView.as_view(
-        template_name='voters/password_reset.html'),
-         name='pass-reset'
+        template_name='voters/password_reset.html', email_template_name="voters/password_reset_email.html", subject_template_name="voters/subject.txt"),
+         name='password_reset'
     ),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
        template_name='voters/password_reset_done.html'),
          name='password_reset_done'
     ),
-    path('password_reset_confirm/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='voters/password_reset_confirm.html'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+       template_name='voters/password_reset_confirm.html'),
          name='password_reset_confirm'
+    ),
+    path('reset/done', auth_views.PasswordResetCompleteView.as_view(
+        template_name='voters/password_reset_complete.html'),
+         name='password_reset_complete'
     ),
 
 
